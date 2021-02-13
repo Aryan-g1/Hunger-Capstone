499e39148dca038e1e2da3363544292cd8f7f6a9
Aryan-g1 <73025091+Aryan-g1@users.noreply.github.com>
2021-02-13 15:05:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var onclick1 = document.getElementById("onclick-a");
+var onclick2 = document.getElementById("onclick-b");
+var onclick3 = document.getElementById("onclick-c");
+var onclick4 = document.getElementById("onclick-d");
+
+onclick1.onclick=()=>{
+    location.href="./index.html";
+    }
+
+onclick2.onclick=()=>{
+    location.href="./what-is-hunger.html";
+    }
+
+onclick3.onclick=()=>{
+    location.href="./food-wastage.html";
+    }
+
+onclick4.onclick=()=>{
+    location.href="./contact-me.html";
+    }
